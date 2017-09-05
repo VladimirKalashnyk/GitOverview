94f38ab2efb00409acb1347365e795246d3107cd
Vladimir Kalashnyk <volodymyrkalashnyk@gmail.com>
2017-09-05 19:32:11 +0300

    Adding new Class BaseEntity

---
+++ src/main/java/com/entity/BaseEntity.java
@@ -0,0 +1,20 @@
+package com.entity;
+
+public class BaseEntity {
+    private  int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "BaseEntity{" +
+                "id=" + id +
+                '}';
+    }
+}
